7dc9d61775e77f26e94ce3caf13a82200c401d80
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-05-27 12:04:04 +0200

    fix: icon page meta

--- src/routes/icons/+page.ts
+++ src/routes/icons/+page.ts
@@ -3,14 +3,14 @@ import type { MetaProps } from 'runes-meta-tags';
 export const load = () => {
   const pageMetaTags = Object.freeze({
     title: 'Icons - Svelte Remix',
-    description: 'Icons page to change colors and sizes',
+    description: 'Explore all SVG icons for our library! Browse and search names, different colors, adjust size, and easily copy icon names with a single click. Find the perfect icon for your project.',
     og: {
       title: 'Icons - Svelte Remix',
-      description: 'Icons page to change colors and sizes'
+      description: 'Explore all SVG icons for our library! Browse and search names, different colors, adjust size, and easily copy icon names with a single click. Find the perfect icon for your project.'
     },
     twitter: {
       title: 'Icons - Svelte Remix',
-      description: 'Icons page to change colors and sizes'
+      description: 'Explore all SVG icons for our library! Browse and search names, different colors, adjust size, and easily copy icon names with a single click. Find the perfect icon for your project.'
     }
   }) satisfies MetaProps;
   return { pageMetaTags };
