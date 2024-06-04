00e6409c28f1adaa6752573ca9d1c606718031c5
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-06-04 06:38:42 +0200

    fix: icon page removing Icon component and remove Object.freeze

--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -3,13 +3,11 @@
   import { IconPage, filterStringKeys } from 'runes-webkit';
   import * as icons from '$lib';
   const keyIcons = filterStringKeys(icons);
-  const filteredIcons = Object.fromEntries(
-    Object.entries(keyIcons).filter(([key]) => key !== 'Icon')
-  );
+
 </script>
 
 <IconPage
-  icons={filteredIcons as Component}
+  icons={keyIcons as Component}
   i_p_tabitem_div_1="md:grid-cols-2 lg:grid-cols-3"
   title="Icons - Svelte Remix"
 />
