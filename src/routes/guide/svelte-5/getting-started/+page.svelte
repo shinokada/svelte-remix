a763f71599ec3421f1fbe1be81160486cb56509f
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-12-06 15:08:13 +0100

    fix: use svelte-shields

--- src/routes/guide/svelte-5/getting-started/+page.svelte
+++ src/routes/guide/svelte-5/getting-started/+page.svelte
@@ -1,5 +1,36 @@
 <script lang="ts">
   import { HighlightCompo, Code, H2 } from 'runes-webkit';
+  import { GitHubSponsor, type GitHubSponsorPropsType,  NpmVersion, type NpmVersionPropsType, License, type LicensePropsType, NpmDownload, type NpmDownloadPropsType } from 'svelte-shields';
+
+  const sponsor: GitHubSponsorPropsType = {
+    user: 'shinokada',
+    logo:'github',
+    color: 'red',
+    link: ['https://github.com/sponsors/shinokada', 'https://github.com/sponsors/shinokada']
+  };
+  const npmversion: NpmVersionPropsType = {
+    packageName: 'svelte-remix',
+    color: 'red',
+    logo:'svelte',
+    link: [
+      'https://www.npmjs.com/package/svelte-remix',
+      'https://www.npmjs.com/package/svelte-remix?activeTab=versions'
+    ]
+  };
+  const license: LicensePropsType = {
+    source: 'github',
+    github_user: 'shinokada',
+    github_repo: 'svelte-remix',
+    link: [
+      'https://github.com/shinokada/svelte-remix/blob/master/LICENSE',
+      'https://github.com/shinokada/svelte-remix/blob/master/LICENSE'
+    ]
+  };
+  const downloads: NpmDownloadPropsType = {
+    packageName: 'svelte-remix',
+    link: ['https://www.npmjs.com/package/svelte-remix']
+  };
+  
   const modules = import.meta.glob('./md/*.md', {
     query: '?raw',
     import: 'default',
@@ -10,37 +41,10 @@
 <h1>Getting Started - Svelte Remix v2</h1>
 
 <div class="my-8 flex gap-2">
-  <a
-    href="https://github.com/sponsors/shinokada"
-    target="_blank"
-    rel="nofollow"
-    aria-label="Sponsor @shinokada"
-    ><img
-      src="https://img.shields.io/static/v1?label=Sponsor&message=%E2%9D%A4&logo=GitHub&color=%23fe8e86"
-      alt="sponsor"
-    /></a
-  >
-  <a
-    href="https://www.npmjs.com/package/svelte-remix"
-    rel="nofollow"
-    target="_blank"
-    aria-label="npm svelte-remix"
-    ><img src="https://img.shields.io/npm/v/svelte-remix/next" alt="npm" /></a
-  >
-  <a
-    href="https://opensource.org/licenses/MIT"
-    rel="nofollow"
-    target="_blank"
-    aria-label="MIT License"
-    ><img src="https://img.shields.io/github/license/shinokada/svelte-remix" alt="License" /></a
-  >
-  <a
-    href="https://www.npmjs.com/package/svelte-remix"
-    rel="nofollow"
-    target="_blank"
-    aria-label="npm svelte-remix"
-    ><img src="https://img.shields.io/npm/dw/svelte-remix.svg" alt="npm" /></a
-  >
+  <GitHubSponsor {...sponsor} />
+  <NpmVersion {...npmversion} />
+  <License {...license} />
+  <NpmDownload {...downloads} />
 </div>
 
 <H2>Requirements</H2>
