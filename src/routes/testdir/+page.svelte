95480fa991a5c3888e391eb84e945d27189fa70d
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-03-10 19:52:13 +0100

    fix: update svelte and tailwindcss 4

---
+++ src/routes/testdir/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { AttachmentLineBusiness } from '$lib';
+  import MyIcon from './MyIcon.svelte'
+</script>
+
+<div class="p-16">
+  <h1>Test</h1>
+  <MyIcon Icon={AttachmentLineBusiness} />
+</div>
